f06d9674a5c559bcdb8a3e91625f5edaaf8057f0
Joel Godinez <jgodinez1114@gmail.com>
2020-02-03 23:48:34 -0600

    Update projects section icons

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+/* script.js
+    Programmer:     Joel Godinez
+    
+    Purpose:        Develop a resume website to showcase projects using Tailwind CSS and vanilla JavaScript
+
+    Environment:    MacOS with Live Server extension
+
+    Files included: index.html, style.css, script.js
+
+    Estimates                Actuals
+     Design: 2 hours          2.5 hours
+     Implementation: 3 hours  4 hours(so far)
+     Testing:        2 hours  1 hour(so far)
+
+
+*/
+
+
 alert('Welcome!')
