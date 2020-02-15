870f0a8bf38795bb0afab86fef6d29fbcd1d4600
jgodinez <jgodinez1114@gmail.com>
2020-02-15 14:21:46 -0600

    Set up Skills buttons to highlight corresponding image(s)

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 /****************************************    script.js    ****************************************************
-    Programmer:     Joel Godinez
-    
-    Purpose:        Develop a resume website to showcase projects using Tailwind CSS and vanilla JavaScript
-
-    Environment:    MacOS with Live Server extension
-
-    Files included: index.html, style.css, script.js
-
-    Estimates                Actuals
-     Design: 2 hours          2.5 hours
-     Implementation: 3 hours  4 hours(so far)
-     Testing:        2 hours  1 hour(so far)
-
+*    Programmer:     Joel Godinez
+*    
+*    Purpose:        Develop a resume website to showcase projects using Tailwind CSS and vanilla JavaScript
+*
+*    Environment:    MacOS with Live Server extension
+*
+*    Files included: index.html, style.css, script.js
+*
+*    Estimates                Actuals
+*     Design: 2 hours          2.5 hours
+*     Implementation: 3 hours  4 hours(so far)
+*     Testing:        2 hours  1 hour(so far)
+*
 **************************************************************************************************************/
 
 
-alert('Hellooo,everybody!')
+// set up data tags and event listeners
+
+const tags = document.querySelectorAll('.tag');
